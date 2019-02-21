4a96334c9c040137269b120e85840b3c5410d913
Kenn <kenn+git@code-crew.org>
2019-02-21 09:03:34 -0600

    update

--- djangocrud_project/census_app/urls.py
+++ djangocrud_project/census_app/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('readres/', views.readRes, name='readRes'),
+    path('createres/', views.createRes, name='createRes'),
+    path('updateres/', views.updateRes, name='updateRes'),
+    path('deleteres/', views.deleteRes, name='deleteRes'),
 ]
--- djangocrud_project/census_app/views.py
+++ djangocrud_project/census_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+# from django.shortcuts import render
+from .models import Respondent
 from django.http import HttpResponse
 import random
 import string
 
+
+# Create your views here.
 
 # Function to generate a random name
 def get_random_name():
@@ -25,3 +25,56 @@ def get_random_salary():
 def index(request):
     return HttpResponse("<h1>Welcome to the Census Application!</h1><h4>" + get_random_name() + "</h4><h4>" + str(
         get_random_age()) + "</h4><h4>" + str(get_random_salary()))
+
+
+def createRes(request):
+    newObject = Respondent(respondent_name = get_random_name(), respondent_age = get_random_age(), respondent_income = get_random_salary())
+    newObject.save()
+    # return HttpResponse(Respondent.objects.all())
+    eachName = ""
+    counter = 0
+    print(Respondent)
+    for eachElement in Respondent.objects.all():
+        eachName += "Name "+  str(counter) + ": " + eachElement.respondent_name + "<br>"
+        counter+=1
+    return HttpResponse(eachName)
+
+
+def readRes(request):
+    eachName = ""
+    counter = 0
+    print(Respondent)
+    for eachElement in Respondent.objects.all():
+        eachName += "Name "+  str(counter) + ": " + eachElement.respondent_name + "<br>"
+        counter+=1
+    return HttpResponse(eachName)
+
+
+def updateRes(request):
+    user = Respondent.objects.get(respondent_name = "Kevin")
+    print(user.respondent_income)
+    user.respondent_income = get_random_salary()
+    user.save()
+    return HttpResponse(user.respondent_income)
+
+
+def deleteRes(request):
+    deleteThisUser = Respondent.objects.get(respondent_name = "Kenn")
+
+    eachName = "BEFORE<br>"
+    counter = 0
+    print(Respondent)
+    for eachElement in Respondent.objects.all():
+        eachName += "Name "+  str(counter) + ": " + eachElement.respondent_name + "<br>"
+        counter+=1
+
+    eachName += "<br><br><br>AFTER<br>"
+
+    deleteThisUser.delete()
+
+    counter = 0
+    for eachElement in Respondent.objects.all():
+        eachName += "Name "+  str(counter) + ": " + eachElement.respondent_name + "<br>"
+        counter+=1
+
+    return HttpResponse(eachName)
